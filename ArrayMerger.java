8719e74f16804afad0670e9f5a541403db63e36b
Wilson.ST <wilsonthehungryman@gmail.com>
2017-09-17 16:05:30 -0500

    Added functionality to merge multiple arrays, even when they are a mix of low to high, high to low.

--- ArrayMerger.java
+++ ArrayMerger.java
@@ -1,5 +1,6 @@
 import java.util.function.IntFunction;
 import java.util.function.BiPredicate;
+import java.util.function.BiFunction;
 
 /**
  * ArrayMerger merges any number of arrays (in the form of a staggered 2d array).
@@ -25,12 +26,19 @@ class ArrayMerger{
         
         // TODO prone to failure/exceptions
         if(arrays[0][0] < arrays[0][arrays[0].length -1])
-            return merge(arrays, outputLowToHigh, true);
+            return performMerge(arrays, outputLowToHigh, true);
         
-        return merge(arrays, outputLowToHigh, false);
+        return performMerge(arrays, outputLowToHigh, false);
     }
     
-    private static int[] merge(int[][] arrays, boolean outputLowToHigh, boolean inputsLowToHigh){
+    public static int[] mergeDifferentOrders(int[][] arrays, boolean outputLowToHigh){ 
+        if(arrays == null)
+            return null;
+            
+        return performMergeDifferentOrder(arrays, outputLowToHigh);
+    }
+    
+    private static int[] performMerge(int[][] arrays, boolean outputLowToHigh, boolean inputsLowToHigh){
         if(arrays.length == 1)
             return singleArrayCase(arrays, outputLowToHigh, inputsLowToHigh);
         
@@ -45,7 +53,7 @@ class ArrayMerger{
         
         // Not sure if the saved equality checks are worth the cost of lambdas...
         BiPredicate<Integer, Integer> comparer = getInnerLoopComparer(inputsLowToHigh);
-        IntFunction<Integer> resultIndex = getResultIndexFunction(inputsLowToHigh == outputLowToHigh, result.length);
+        IntFunction<Integer> resultIndex = getIndexFunction(inputsLowToHigh == outputLowToHigh, result.length);
 
         // loop through the results array, appending one value each time
         for(int i = 0; i < result.length; i++){
@@ -74,6 +82,52 @@ class ArrayMerger{
         return result;
     }
     
+    private static int[] performMergeDifferentOrder(int[][] arrays,
+                                                    boolean outputLowToHigh){
+        if(arrays.length == 1)
+            return singleArrayCase(arrays, outputLowToHigh, arrays[0][0] <= arrays[0][arrays[0].length -1]);
+        
+        int[] currentValues = new int[arrays.length];
+        int[] pointers = new int[arrays.length];
+        
+        // TODO - maybe better as a boolean list (with comparison before assigning next currentValue)
+        IntFunction<Integer>[] indexMap = new IntFunction[arrays.length];
+        
+        // Initializes pointers, currentValues, and then returns the results array
+        int[] result = initialize(arrays, pointers, currentValues, indexMap);
+        
+        // length = number of subarrays
+        int length = currentValues.length;
+        
+        // Not sure if the saved equality checks are worth the cost of lambdas...
+        IntFunction<Integer> resultIndex = getIndexFunction(outputLowToHigh, result.length);
+
+        // loop through the results array, appending one value each time
+        for(int i = 0; i < result.length; i++){
+            int current = currentValues[0];
+            int currentPointer = 0;
+            
+            for(int j = 1; j < length; j++){
+                if(currentValues[j] < current){
+                    current = currentValues[j];
+                    currentPointer = j;
+                }
+            }
+            
+            result[resultIndex.apply(i)] = current;
+            pointers[currentPointer] += 1;
+            
+            // if the current pointer is at the end of the array, remove that subarray
+            if(pointers[currentPointer] == arrays[currentPointer].length){
+                shrink(currentPointer, arrays, pointers, currentValues, indexMap);
+                length--;
+            }else
+                // Otherwise, "move" the pointer
+                currentValues[currentPointer] = arrays[currentPointer][indexMap[currentPointer].apply(pointers[currentPointer])];
+        }
+        return result;
+    }
+    
     private static int[] initialize(int[][] arrays, int[] pointers, int[] currentValues){
         int totalLength = 0;
         
@@ -85,11 +139,37 @@ class ArrayMerger{
         return new int[totalLength];
     }
     
+    private static int[] initialize(int[][] arrays, int[] pointers, int[] currentValues,
+                                    IntFunction<Integer>[] indexMap){
+                                    // BiFunction<Integer, int[], Integer>[] indexMap){
+        int totalLength = 0;
+        
+        // TODO will fail if subarray is null
+        for(int i = 0; i < arrays.length; i++){
+            totalLength += arrays[i].length;
+            currentValues[i] = arrays[i][0];
+            if(arrays[i][arrays[i].length -1] < currentValues[i])
+                indexMap[i] = getIndexFunction(false, arrays[i].length); 
+            else
+                indexMap[i] = getIndexFunction(true, arrays[i].length);
+        }
+        return new int[totalLength];
+    }
+    
     private static void shrink(int pointer, int[][] arrays, int[] pointers, int[] values){
         for(int i = pointer + 1; i < pointers.length; i++){
             pointers[i -1] = pointers[i];
             values[i -1] = values[i];
             arrays[i -1] = arrays[i];
+        }
+    }
+    
+    private static void shrink(int pointer, int[][] arrays, int[] pointers, int[] values, IntFunction[] indexMap){
+        for(int i = pointer + 1; i < pointers.length; i++){
+            pointers[i -1] = pointers[i];
+            values[i -1] = values[i];
+            arrays[i -1] = arrays[i];
+            indexMap[i -1] = indexMap[i];
         }
     }
     
@@ -112,7 +192,7 @@ class ArrayMerger{
             return (current, other) -> current > other;
     }
     
-    private static IntFunction getResultIndexFunction(boolean sameOrder, int resultLength){
+    private static IntFunction<Integer> getIndexFunction(boolean sameOrder, int resultLength){
         if(sameOrder)
             return i -> i;
         return i -> { return resultLength - 1 - i; };
